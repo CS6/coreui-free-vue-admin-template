76400c5665484124f4686cb4bdf9463c136b38d9
fixiabis <fixiabis@gmail.com>
2019-06-10 14:54:27 +0800

    auth feature complete

--- src/main.js
+++ src/main.js
@@ -3,11 +3,12 @@
 import 'core-js/es6/promise'
 import 'core-js/es6/string'
 import 'core-js/es7/array'
-// import cssVars from 'css-vars-ponyfill'
+import cssVars from 'css-vars-ponyfill'
 import Vue from 'vue'
 import BootstrapVue from 'bootstrap-vue'
 import App from './App'
 import router from './router'
+
 import firebase from 'firebase/app'
 import 'firebase/auth'
 
@@ -20,10 +21,10 @@ var firebaseConfig = {
   storageBucket: "docker-console.appspot.com",
   messagingSenderId: "850453417660",
   appId: "1:850453417660:web:b50f9c7067ed91a3"
-}
+};
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig)
+firebase.initializeApp(firebaseConfig);
 
 // todo
 // cssVars()
@@ -40,6 +41,50 @@ new Vue({
   }
 })
 
-debug: (function (global) {
-  global.firebase = firebase
-})(window)
+main: (function (global) {
+  global.firebase = firebase; // 前端console偵錯
+
+  firebase.auth().getRedirectResult().then(function (result) {
+    var user = result.user;
+    if (!user) signIn();
+  }).catch(function (error) {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    var email = error.email;
+    var credential = error.credential;
+
+    if (errorCode === 'auth/account-exists-with-different-credential') {
+      alert('You have already signed up with a different auth provider for that email.');
+    } else {
+      console.error(error);
+    }
+  });
+
+  var url = "URL_HERE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!";
+
+  firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+      var displayName = user.displayName;
+      var email = user.email;
+      var emailVerified = user.emailVerified;
+      var photoURL = user.photoURL;
+      var isAnonymous = user.isAnonymous;
+      var uid = user.uid;
+      var providerData = user.providerData;
+
+      console.log(uid); // 使用者登入後的uid在此
+      location = `${url}?uid=${uid}`;
+    } else signIn();
+  });
+
+  function signIn() { // 登入
+    var provider = new firebase.auth.GoogleAuthProvider();
+    provider.addScope('https://www.googleapis.com/auth/plus.login');
+    provider.setCustomParameters({ prompt: 'select_account' });
+    firebase.auth().signInWithRedirect(provider);
+  }
+
+  function signOut() { // 登出
+    firebase.auth().signOut();
+  }
+})(window);
